1f56003753558f29baa5add88ca6266b3a109c4d
Leonardo Nunes <leonardo-junior@users.noreply.github.com>
2022-06-23 23:44:57 -0300

    feat: add missing random numbers component

---
+++ src/pages/random-number.tsx
@@ -0,0 +1,13 @@
+// components
+import Layout from '../components/Layout/Layout'
+import Random from '../components/Random/Random'
+
+function RandomPage ():JSX.Element {
+  return (
+    <Layout title='Sorteio numero' description='A simple page'>
+      <Random />
+    </Layout>
+  )
+}
+
+export default RandomPage
