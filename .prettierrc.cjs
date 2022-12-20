f34b56ad8967876e9bba6e2ed0411f7b68e57fd8
Leonardo Nunes <leonardo-junior@users.noreply.github.com>
2022-12-20 13:40:41 -0300

    feat: config eslint

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  "arrowParens": "always",
+  "endOfLine": "auto",
+  "printWidth": 110,
+  "quoteProps": "preserve",
+  "semi": false,
+  "singleQuote": true,
+  "tabWidth": 2,
+  "trailingComma": "es5",
+  "useTabs": false,
+};
