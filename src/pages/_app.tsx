1a22524d7798c593ee1733925b11357543eacb9f
Leonardo Nunes <leonardo-junior@users.noreply.github.com>
2022-12-23 23:57:45 -0300

    feat: remove theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,16 @@
 // vendors
-import { ThemeProvider } from 'next-themes'
+// import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
 
 // styles
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  // TODO create theme
   return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    // <ThemeProvider>
+    <Component {...pageProps} />
+    // </ThemeProvider>
   )
 }
 
