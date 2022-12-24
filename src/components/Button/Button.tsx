107a30718272d5880d1caa320dbe28e5bd686238
Leonardo Nunes <leonardo-junior@users.noreply.github.com>
2022-12-24 18:43:23 -0300

    feat: add icon option on Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,22 @@
 // styles
+import clsx from 'clsx'
 import styles from './Button.module.scss'
 
 type ButtonProps = {
   text?: string
   children?: JSX.Element
+  icon?: boolean
 } & React.HTMLAttributes<HTMLButtonElement>
 
-export const Button = ({ text, children, ...props }: ButtonProps): JSX.Element => {
+export const Button = ({ text, children, icon = false, ...props }: ButtonProps): JSX.Element => {
   return (
-    <button className={styles.button} {...props}>
+    <button
+      className={clsx({
+        [styles.button]: true,
+        [styles.icon]: icon,
+      })}
+      {...props}
+    >
       {text && text}
 
       {children && children}
