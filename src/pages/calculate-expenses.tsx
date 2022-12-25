d4e552b00728ae5cfdf0b77eb73b5e850450aa8d
Leonardo Nunes <leonardo-junior@users.noreply.github.com>
2022-12-25 12:45:38 -0300

    fix: remove unnecessary datas

--- src/pages/calculate-expenses.tsx
+++ src/pages/calculate-expenses.tsx
@@ -1,46 +1,13 @@
-// vendors
-import { useState } from 'react'
-
 // components
 import { Layout } from '../components/Layout/Layout'
-import { Expense } from '../components/ShareExpenses/Expense/Expense'
-import { Home } from '../components/ShareExpenses/Home/Home'
+import { ShareExpenses } from '../components/ShareExpenses/ShareExpenses'
 
-function ShareExpenses(): JSX.Element {
-  const [showFirstInfo, setShowFirstInfo] = useState(true)
-  const [showCaculate, setShowCalculate] = useState(false)
-  const [showResult, setShowResult] = useState(false)
-
-  function showInfos() {
-    setShowFirstInfo(true)
-    setShowCalculate(false)
-    setShowResult(false)
-  }
-
-  function showCalculate() {
-    setShowFirstInfo(false)
-    setShowCalculate(true)
-    setShowResult(false)
-  }
-
-  function showResults() {
-    setShowFirstInfo(false)
-    setShowCalculate(false)
-    setShowResult(true)
-  }
-
+function ShareExpensesPage(): JSX.Element {
   return (
     <Layout title="Divisão gastos" description="Arquitetura de divisão de gastos">
-      <button onClick={showInfos}>first</button>
-      <button onClick={showCalculate}>Second</button>
-      <button onClick={showResults}>Third</button>
-      {showFirstInfo && <Home />}
-
-      {showCaculate && <Expense />}
-
-      {/* {showResult && <ResultCard />} */}
+      <ShareExpenses />
     </Layout>
   )
 }
 
-export default ShareExpenses
+export default ShareExpensesPage
